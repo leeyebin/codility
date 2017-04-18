76cab1ce7d3a52159d80be8cc7d74218b6a22837
leeyebin <leeyebin007@gmail.com>
2017-04-18 12:33:37 +0900

    binary

--- binary/MySolution.java
+++ binary/MySolution.java
@@ -1,26 +1,26 @@
 class Solution {
-	    public int solution(int N) {
-	    	int Count = 0;
+    public int solution(int N) {
+        int Count = 0;
 	    	int tmpCount = 0;
-	    	boolean chk;
+	    	boolean chk=true;
 	    	
 	    	if(N%2==0){
-	    		chk=false;
+	    		chk = false;
 	    	}
+	    	
 	    	while(N!=1){
 	    		if(N%2==1){
 	    			N/=2;
-	    			if(tmpCount>Count && chk==true){
-	    				Count = tmpCount;
-	    			}
 	    			tmpCount = 0 ;
 	    			chk = true;
 	    		}else if(N%2==0){
 	    			N/=2;
 	    			tmpCount++;
+	    			if(tmpCount>Count&&chk==true){
+	    				Count = tmpCount;
+	    			}
 	    		}
 	    	}
-	        
-	        return Count;
-	    }
-	}
+	    	return Count;
+    }
+}
