b6bcae91926bb4a175777d90cee755dd8810d31a
leeyebin <leeyebin007@gmail.com>
2017-04-18 11:39:49 +0900

    binary

--- binary/MySolution.java
+++ binary/MySolution.java
@@ -1,19 +1,23 @@
 class Solution {
 	    public int solution(int N) {
-	    	// write your code in Java SE 8
-	        int Count = 0;
+	    	int Count = 0;
 	    	int tmpCount = 0;
+	    	boolean chk=false;
 	    	
+	    	if(N%2==0){
+	    		chk=false;
+	    	}
 	    	while(N!=1){
 	    		if(N%2==1){
 	    			N/=2;
+	    			if(tmpCount>Count && chk==true){
+	    				Count = tmpCount;
+	    			}
 	    			tmpCount = 0 ;
+	    			chk = true;
 	    		}else if(N%2==0){
 	    			N/=2;
 	    			tmpCount++;
-	    			if(tmpCount>Count){
-	    				Count = tmpCount;
-	    			}
 	    		}
 	    	}
 	        
