33a85cf29d62f5760f261a6503402e0d3b955a69
leeyebin <leeyebin007@gmail.com>
2017-05-02 16:34:51 +0900

    https://codility.com/demo/results/trainingWFYC4N-355

---
+++ FrogRiverOne/MySolution.java
@@ -0,0 +1,21 @@
+﻿import java.util.HashSet;
+
+class Solution {
+    public int solution(int X, int[] A) {
+        HashSet<Integer> set = new HashSet<Integer>();
+		for(int j=1; j<=X; j++){
+			set.add(j);
+		}
+			
+		for(int i=0; i<A.length; i++){
+			if(A[i] <= X){
+				set.remove(A[i]);
+			}
+			if(set.isEmpty()){
+				return i;
+			}
+		}		
+		
+		return -1;
+    }
+}
