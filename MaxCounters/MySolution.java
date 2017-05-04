297129f70cfe790852d65f104e64bb110c18cb06
Yebin Lee <leeyebin007@gmail.com>
2017-05-05 02:12:44 +0900

    https://codility.com/demo/results/trainingU5N769-B2N

---
+++ MaxCounters/MySolution.java
@@ -0,0 +1,20 @@
+﻿class Solution {
+    public int[] solution(int N, int[] A) {
+    	int max = 0;
+    	int[] maxCounter = new int[N];
+    	
+    	for(int i=0; i<A.length; i++){
+    		if(A[i]>=1&&A[i]<=N){
+    			maxCounter[A[i]-1]++;
+    			if(maxCounter[A[i]-1]>max){
+    				max = maxCounter[A[i]-1]; 
+    			}
+    		}else if(A[i]==N+1){
+    			for(int j=0; j<maxCounter.length; j++){
+    				maxCounter[j]=max;
+    			}
+    		}
+    	}
+        return maxCounter;
+    }
+}
