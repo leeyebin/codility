8f513b6548977f0d1540bca43d51bcabe0e94aa2
leeyebin <leeyebin007@gmail.com>
2017-05-18 15:00:30 +0900

    https://codility.com/demo/results/trainingGVEKU4-VFV

---
+++ Triangle/MySolution.java
@@ -0,0 +1,15 @@
+﻿import java.util.Arrays;
+
+
+class Solution {
+    public int solution(int[] A) {
+        Arrays.sort(A);
+        
+        for(int i=0; i<A.length-2; i++){
+        	if(A[i]+A[i+1]>A[i+2]&&A[i+1]+A[i+2]>A[i]&&A[i]+A[i+2]>A[i+1]){
+        		return 1;
+        	}
+        }
+		return 0;
+    }
+}
