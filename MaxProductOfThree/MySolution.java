c27bfcdec3081f405d79b929016e15a1bbedc37e
leeyebin <leeyebin007@gmail.com>
2017-05-19 16:08:10 +0900

    https://codility.com/demo/results/trainingYW42CY-5CA

---
+++ MaxProductOfThree/MySolution.java
@@ -0,0 +1,26 @@
+﻿import java.util.Arrays;
+
+
+class Solution {
+    public int solution(int[] A) {
+        Arrays.sort(A);
+        int start=0;
+        int end = A.length-1;
+        
+        if(A[start]>=0){
+        	return A[end]*A[end-1]*A[end-2];
+        }
+        if(A[end]<=0){
+        	return A[end]*A[end-1]*A[end-2];
+        }
+        if(A[start]<=0&&A[end]>0){
+        	if(A[start]*A[start+1]>A[end-1]*A[end-2]){
+        		return A[start]*A[start+1]*A[end];
+        	}else{
+        		return A[end]*A[end-1]*A[end-2];
+        	}
+        }
+        
+        return 0;
+    }
+}
