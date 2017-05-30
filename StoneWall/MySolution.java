e7fbb8921047c2d8da12e1bb5a19d947586a1fcb
leeyebin <leeyebin007@gmail.com>
2017-05-30 11:20:27 +0900

    https://codility.com/demo/results/trainingSEZWDN-8F3

---
+++ StoneWall/MySolution.java
@@ -0,0 +1,35 @@
+﻿import java.util.Stack;
+
+class Solution {
+	public int solution(int[] H) {
+		Stack<Integer> stack = new Stack<Integer>();
+		int count = 0;
+		for(int i=0; i<H.length; i++){
+			if(i==0){
+				stack.push(H[0]);
+				count++;
+			}
+			
+			if(stack.peek()<H[i]){
+				stack.push(H[i]);
+				count++;
+			}else if(stack.peek()>H[i]){
+				while(!stack.isEmpty()&&stack.peek()>H[i]){
+						stack.pop();
+				}
+				
+				if(stack.size()!=0){
+					if(stack.peek()!=H[i]){
+						stack.push(H[i]);
+						count++;
+					}
+				}else{
+					stack.push(H[i]);
+					count++;
+				}
+			}
+		}
+		
+		return count;
+    }
+}
